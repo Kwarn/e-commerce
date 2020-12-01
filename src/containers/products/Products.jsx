8807a7cd59d410f42a41105482c889c8ee453ea9
Kwarn <karlwarner.dev@gmail.com>
2020-12-01 11:31:43 +0000

    products heading and TextSection added

--- src/containers/products/Products.jsx
+++ src/containers/products/Products.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ImageSection from '../../components/ImageSection/ImageSection';
+import products from '../../assets/products.jpg';
 import hardwood from '../../assets/hardwood.jpg';
 import laminate from '../../assets/laminate.jpg';
+import TextSection from '../../components/textSection/TextSection';
 
 export default function Products() {
   const productMenuItems = { Hardwood: hardwood, Laminate: laminate };
@@ -15,5 +17,14 @@ export default function Products() {
     />
   ));
 
-  return menuItems;
+  return (
+    <>
+      <ImageSection image={products} shouldFillWidth={true} />
+      <TextSection
+        h1="OUR PRODUCTS"
+        p="Make a house a home with our diverse range of products"
+      />
+      {menuItems}
+    </>
+  );
 }
