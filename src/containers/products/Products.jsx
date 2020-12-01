fb0f056d35d980683141b92ed819911f370be32f
Kwarn <karlwarner.dev@gmail.com>
2020-12-01 10:56:20 +0000

    basic menuItems added for Product catagories

--- src/containers/products/Products.jsx
+++ src/containers/products/Products.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import ImageSection from '../../components/ImageSection/ImageSection';
+import hardwood from '../../assets/hardwood.jpg';
+import laminate from '../../assets/laminate.jpg';
 
 export default function Products() {
-  return <div>Products</div>;
+  const productMenuItems = { Hardwood: hardwood, Laminate: laminate };
+
+  const menuItems = Object.keys(productMenuItems).map(key => (
+    <ImageSection
+      key={key}
+      title={`${key} flooring`}
+      buttonText="BROWSE"
+      image={productMenuItems[key]}
+    />
+  ));
+
+  return menuItems;
 }
