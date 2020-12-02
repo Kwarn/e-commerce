44048a1940464e060ef2c23585d0fadc795d208c
Kwarn <karlwarner.dev@gmail.com>
2020-12-02 13:36:32 +0000

    Highlighting based on current route added, background image & closeButton added

--- src/components/navigation/Toolbar/Sidebar/Sidebar.jsx
+++ src/components/navigation/Toolbar/Sidebar/Sidebar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavigationItems from '../../NavigationItems/NavigationItems';
 import Backdrop from '../../../UI/backdrop/Backdrop';
 import Logo from '../../../../assets/logo.png';
 import styled, { css } from 'styled-components';
+import { useLocation, withRouter } from 'react-router-dom';
+import background from '../../../../assets/sidebarBackground.jpg';
+import closeButton from '../../../../assets/closeButton.png';
 
 const StyledSideDraw = styled.div`
+  position: relative;
+  background-image: url(${background});
+  background-size: cover;
   position: fixed;
   width: 280px;
-  max-width: 70%;
+  max-width: 80%;
   height: 100%;
   left: 0;
   top: 0;
@@ -17,6 +23,7 @@ const StyledSideDraw = styled.div`
   box-sizing: border-box;
   transition: transform 0.3s ease-out;
   .logo {
+    text-align: center;
     height: 100px;
     width: auto;
   }
@@ -33,19 +40,76 @@ const StyledSideDraw = styled.div`
     `}
 `;
 
-const sideDraw = ({ isOpen, closeFn }) => {
+const StyledCloseButton = styled.img`
+  cursor: pointer;
+  height: 3vh;
+  width: auto;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+const StyledMenuContainer = styled.div`
+  margin-top: 20%;
+`;
+
+const StyledMenuItem = styled.div`
+  margin: auto;
+  width: 80%;
+  height: 30px;
+  h1 {
+    color: white;
+    text-align: center;
+    font-size: 1.3em;
+    font-weight: lighter;
+  }
+  border-bottom: 4px solid ${props => (props.isCurrentPage ? '#eee' : 'none')};
+`;
+
+const SideDraw = ({ isOpen, closeFn, history }) => {
+  const userPageLocation = useLocation();
+
+  const changeRouteHandler = pageId => {
+    history.push(`/${pageId}`);
+  };
+
   return (
     <>
       <Backdrop show={isOpen} closeFn={closeFn} />
       <StyledSideDraw onClick={closeFn} show={isOpen}>
-        <div>
-          <img className="logo" src={Logo} alt="Twelve Oak" />
-        </div>
-        <div>Products</div>
-        <div>Contact</div>
+        <StyledCloseButton src={closeButton} alt="close"></StyledCloseButton>
+        <StyledMenuContainer>
+          <StyledMenuItem
+            onClick={() => changeRouteHandler('products')}
+            isCurrentPage={userPageLocation.pathname === '/products'}
+          >
+            <h1>OUR PRODUCTS</h1>
+          </StyledMenuItem>
+          <StyledMenuItem
+            onClick={() => changeRouteHandler('about')}
+            isCurrentPage={userPageLocation.pathname === '/about'}
+          >
+            <h1>ABOUT</h1>
+          </StyledMenuItem>
+          <StyledMenuItem
+            onClick={() => changeRouteHandler('contact')}
+            isCurrentPage={userPageLocation.pathname === '/contact'}
+          >
+            <h1>CONTACT</h1>
+          </StyledMenuItem>
+        </StyledMenuContainer>
       </StyledSideDraw>
     </>
   );
 };
 
-export default sideDraw;
+export default withRouter(SideDraw);
+
+// <div>
+//   <img
+//     className="logo"
+//     src={Logo}
+//     alt="Twelve Oak"
+//     onClick={() => history.push('/home')}
+//   />
+// </div>;
