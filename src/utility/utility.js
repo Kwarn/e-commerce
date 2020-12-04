91c476c87f1156aa36c49ee8df1b0993deaa700c
Kwarn <karlwarner.dev@gmail.com>
2020-12-04 13:55:20 +0000

    validateInput and updateObject helper functions added

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,3 +1,19 @@
-export default {};
+export const updateObject = (oldObject, updatedProperties) => {
+  return {
+    ...oldObject,
+    ...updatedProperties,
+  };
+};
 
-export const route;
+export const validateInput = (value, rules) => {
+  let isValid = true;
+  if (rules.required) isValid = value.trim() !== '' && isValid;
+  if (rules.minLength) isValid = value.length >= rules.minLength && isValid;
+  if (rules.maxLength) isValid = value.length <= rules.maxLength && isValid;
+  if (rules.isNumeric) isValid = /^\d+$/.test(value) && isValid;
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid = pattern.test(value) && isValid;
+  }
+  return isValid;
+};
