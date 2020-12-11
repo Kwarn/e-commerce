1d21e354585bef92eea6d5604961eff56fc57c0e
Kwarn <karlwarner.dev@gmail.com>
2020-12-11 12:06:19 +0000

    creation of image/textSections moved to their own file, destructuring of new returned ImageSections implimented.

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,53 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import ImageSection from '../../components/ImageSection/ImageSection';
-import welcomeImage from '../../assets/welcomeImage.jpg';
-import flooring1 from '../../assets/flooring1.jpg';
-import happyCustomer from '../../assets/happyCustomer.jpg';
-import TextSection from '../../components/textSection/TextSection';
+import TextSections from '../TextSections/TextSections';
+import ImageSections from '../ImageSections/ImageSections';
 import { withRouter } from 'react-router-dom';
+import { mediaQueries } from '../../mediaQueries/mediaQueries';
 
 const StyledHomeWrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 100%;
+  align-items: center;
 `;
 
 function Home(props) {
-  const showSettings = e => {
-    e.preventDefault();
-    console.log('works');
-  };
-  const buttonCallbackHandler = route => {};
-
+  const { welcomeElement, testimonials, aboutUs } = ImageSections();
   return (
     <StyledHomeWrapper>
-      <ImageSection
-        title="NATURAL WOOD FLOORING"
-        description="Discover our extensive range"
-        buttonText="Explore"
-        buttonCallback={() => props.history.push('/products')}
-        shouldFillWidth={true}
-        image={welcomeImage}
-      ></ImageSection>
-      <TextSection
-        h1="Find Your Perfect Wood Floor"
-        p="We make beautiful real wood flooring from responsibly sourced timber"
-      />
-      <ImageSection
-        title="OUR HAPPPY CUSTOMERS"
-        description="What they have to say"
-        buttonText="Testimonials"
-        buttonCallback={() => props.history.push('/testimonials')}
-        image={happyCustomer}
-      ></ImageSection>
-      <ImageSection
-        title="ABOUT US"
-        description="Meet our specialists"
-        buttonText="FIND OUT MORE"
-        buttonCallback={() => props.history.push('/about')}
-        image={flooring1}
-      ></ImageSection>
+      {welcomeElement}
+      {testimonials}
+      {aboutUs}
     </StyledHomeWrapper>
   );
 }
