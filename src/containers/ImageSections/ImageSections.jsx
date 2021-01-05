bfe762544e5d6f7bc700323c956ecda638e3b4ee
Kwarn <karlwarner.dev@gmail.com>
2021-01-05 10:51:03 +0000

    images updated for image sections. Grammar corrections.

--- src/containers/ImageSections/ImageSections.jsx
+++ src/containers/ImageSections/ImageSections.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import welcomeImage from '../../assets/welcomeImage.jpeg';
+// import welcomeImage from '../../assets/welcomeImage.jpeg';
+import goldFloor from '../../assets/goldFloor.jpeg';
 import ImageSection from './ImageSection/ImageSection';
 import flooring1 from '../../assets/flooring1.jpg';
 import happyCustomer from '../../assets/happyCustomer.jpg';
+import welcomeImage from '../../assets/withChair.jpg';
 import { withRouter, useHistory } from 'react-router-dom';
 
 const ImageSections = () => {
@@ -11,15 +13,14 @@ const ImageSections = () => {
     welcomeElement: {
       title: 'NATURAL WOOD FLOORING',
       description: 'Discover our extensive range',
-      textColor: 'white',
       buttonText: 'Explore',
       buttonCallback: () => history.push('/products'),
       shouldFillWidth: true,
       image: welcomeImage,
     },
     testimonials: {
-      title: 'OUR HAPPPY CUSTOMERS',
-      description: 'What they have to say',
+      title: 'OUR CUSTOMERS',
+      description: 'Hear what they have to say',
       buttonText: 'Testimonials',
       buttonCallback: () => history.push('/testimonials'),
       image: happyCustomer,
@@ -27,9 +28,9 @@ const ImageSections = () => {
     aboutUs: {
       title: 'ABOUT US',
       description: 'Meet our specialists',
-      buttonText: 'FIND OUT MORE',
+      buttonText: 'Find out more',
       buttonCallback: () => history.push('/about'),
-      image: flooring1,
+      image: goldFloor,
     },
   };
 
