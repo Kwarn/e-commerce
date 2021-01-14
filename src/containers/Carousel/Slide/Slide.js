92bb66c2ad1ad6ee1eeba379de51132d3df26db0
Kwarn <karlwarner.dev@gmail.com>
2021-01-14 13:37:54 +0000

    call to action central slide content added, css & event handling fixed

--- src/containers/Carousel/Slide/Slide.js
+++ src/containers/Carousel/Slide/Slide.js
@@ -3,25 +3,62 @@ import styled from 'styled-components';
 import LayoutsContext from '../../../Layout/LayoutsContext';
 
 const StyledSlide = styled.div`
+  position: relative;
   margin: auto;
-  color: #474747;
-  max-width: 100%;
-  text-align: center;
+  display: flex;
+  min-width: 100%;
+  min-height: 100%;
+  background-image: url(${({ background }) => background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 0;
 `;
 
-const StyledImage = styled.img`
-  min-height: ${props =>
-    props.isMobile ? '86vh' : props.isTablet ? '88vh' : '90vh'};
-  width: 100%;
-  object-fit: cover;
+const StyledContentWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  top: 50%;
+  margin-top: -60px;
+  min-height: 120px;
+  left: 50%;
+  color: ${props => (props.isImageDark ? 'black' : 'white')};
+  margin-left: ${props =>
+    props.isMobile ? '-120px' : props.isTablet ? '-150px' : '-250px'};
+  width: ${props =>
+    props.isMobile ? '240px' : props.isTablet ? '300px' : '500px'};
+  background-color: ${props =>
+    props.isImageDark ? 'rgba(225, 225, 225, 0.5)' : 'rgba(0, 0, 0, 0.5)'};
+`;
+
+const StyledCallToAction = styled.div`
+  cursor: pointer;
+  display: flex;
+  margin: auto;
+  height: 60px;
+  width: ${props =>
+    props.isMobile ? '180px' : props.isTablet ? '200px' : '420px'};
+  border: ${props =>
+    props.isImageDark ? '2px solid black' : '2px solid white'};
+`;
+
+const StyledText = styled.div`
+  margin: auto;
+  text-align: center;
 `;
 
 const Slide = ({ slide }) => {
   const layouts = useContext(LayoutsContext);
-
   return (
-    <StyledSlide>
-      <StyledImage {...layouts} src={slide.image} />
+    <StyledSlide {...layouts} background={slide.image}>
+      <StyledContentWrapper {...layouts} isImageDark={slide.isImageDark}>
+        <StyledCallToAction
+          onClick={slide.callbackFn}
+          {...layouts}
+          isImageDark={slide.isImageDark}
+        >
+          <StyledText>{slide.title}</StyledText>
+        </StyledCallToAction>
+      </StyledContentWrapper>
     </StyledSlide>
   );
 };
