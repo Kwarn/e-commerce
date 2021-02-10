1686250a257c67a1be9ec34665597bf9e772499f
Kwarn <karlwarner.dev@gmail.com>
2021-02-10 12:57:33 +0000

    Button: Parameter added to allow dark/light text

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,11 +7,16 @@ const StyledButton = styled.button`
   width: fit-content;
   font-size: 1.2em;
   height: 100%;
-  color: white;
-  border: 2px solid white;
-  background-color: rgba(0, 0, 0, 0.2);
+  color: ${props => (props.isDarkText ? '#474747' : '#eee')};
+  border: 1px solid ${props => (props.isDarkText ? '#474747' : '#eee')};
+  background-color: ${props =>
+    props.isDarkText ? 'rgba(255,255,255,0.2)' : 'rgba(0, 0, 0, 0.2)'}; ;
 `;
 
-export default function Button({ text, callback }) {
-  return <StyledButton onClick={callback}>{text}</StyledButton>;
+export default function Button({ text, callback, isDarkText }) {
+  return (
+    <StyledButton isDarkText={isDarkText} onClick={callback}>
+      {text}
+    </StyledButton>
+  );
 }
