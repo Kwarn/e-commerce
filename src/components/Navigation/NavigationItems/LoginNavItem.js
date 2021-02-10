6f10379a0f4582c5845dcfee2ecac19b8da3095e
Kwarn <karlwarner.dev@gmail.com>
2021-02-10 12:58:55 +0000

    NavigationItems: Login: component animations revamped

--- src/components/Navigation/NavigationItems/LoginNavItem.js
+++ src/components/Navigation/NavigationItems/LoginNavItem.js
@@ -4,28 +4,41 @@ import { validateInput } from '../../../utility/utility';
 import Spinner from '../../UI/spinner/Spinner';
 import styled from 'styled-components';
 import LayoutsContext from '../../../Layout/LayoutsContext';
+import background from '../../../assets/sidebarBackground.jpg';
+import Button from '../../Button/Button';
 
 const StyledDrawContainer = styled.div`
+  color: #474747;
   position: fixed;
   display: flex;
   flex-direction: column;
   z-index: 5;
-  background-color: #ccc;
-  width: 30vw;
-  height: 30vh;
+  border-radius: 0 0 150px 0;
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) -10%,
+    rgba(173, 93, 50, 1) 97%
+  );
+  width: 20vw;
   right: 0;
-  transition: top 0.6s;
-  top: ${props =>
-    props.isNavBarMinimized
-      ? props.isHidden
-        ? '-30vh'
-        : '10vh'
-      : props.isHidden
-      ? '-30vh'
-      : '20vh'};
+  height: 100%;
+  max-height: ${props => (props.isHidden ? '0vh' : '30vh')};
+  top: ${props => (props.isNavBarMinimized ? '10vh' : '20vh')};
+  transition: top 0.6s, max-height 0.6s ease-in-out;
+  overflow: hidden;
 `;
 
-const StyledLoginSwitchMesssage = styled.h2`
+const StyledDrawElementsWrapperForAnimation = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  opacity: ${props => (props.isHidden ? 0 : 1)};
+  transition: opacity 1s ease-in-out;
+`;
+
+const StyledLoginSwitchMesssage = styled.h3`
   height: 20%;
   text-align: center;
   margin: 5vh auto 0 auto;
@@ -35,7 +48,7 @@ const StyledLoginSwitchButton = styled.div`
   cursor: pointer;
   text-decoration: underline;
   width: fit-content;
-  margin: 0 auto 0 auto;
+  margin: -15px auto 0 auto;
 `;
 
 const StyledForm = styled.form`
@@ -46,11 +59,20 @@ const StyledForm = styled.form`
 `;
 
 const StyledInput = styled.input`
-  margin: auto;
+  margin: 0 auto 5px auto;
+  padding: 5px;
+  height: 20px;
   width: 80%;
 `;
-const StyledButton = styled.button`
-  margin: auto;
+
+const StyledSubmitButtonContainer = styled.div`
+  margin: 5px auto 10px auto;
+`;
+
+const StyledCloseButtonContainer = styled.div`
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
 `;
 
 const LoginNavItem = ({ toggleCallback, isHidden, scrollPos }) => {
@@ -125,13 +147,13 @@ const LoginNavItem = ({ toggleCallback, isHidden, scrollPos }) => {
   let formElements = formElementsArray.map(elem => (
     <StyledInput
       key={elem.id}
+      placeholder={elem.config.elementConfig.placeholder}
       elementType={elem.config.elementType}
-      elementConfig={elem.config.elementConfig}
-      value={elem.config.value || elem.config.placeholder}
+      value={elem.config.value}
       invalid={elem.config.valid}
       shouldValidate={elem.config.validation}
       touched={elem.config.touched}
-      changed={event => inputChangedHandler(event, elem.id)}
+      onChange={event => inputChangedHandler(event, elem.id)}
     />
   ));
 
@@ -143,17 +165,33 @@ const LoginNavItem = ({ toggleCallback, isHidden, scrollPos }) => {
       {...layouts}
       isHidden={isHidden}
     >
-      <StyledLoginSwitchMesssage>
-        {isSignup ? 'Already Have an account?' : `Don't have an account yet?`}
-      </StyledLoginSwitchMesssage>
-      <StyledLoginSwitchButton onClick={switchAuthModeHandler} btnType="Danger">
-        {isSignup ? 'Switch To Login' : 'Sign Up!'}
-      </StyledLoginSwitchButton>
-      <StyledForm onSubmit={submitHandler}>
-        {formElements}
-        <StyledButton btnType="Success">Submit</StyledButton>
-      </StyledForm>
-      <StyledButton onClick={() => toggleCallback()}>Close</StyledButton>
+      <StyledDrawElementsWrapperForAnimation isHidden={isHidden}>
+        <StyledLoginSwitchMesssage>
+          {isSignup ? 'Already Have an account?' : `Don't have an account yet?`}
+        </StyledLoginSwitchMesssage>
+        <StyledLoginSwitchButton
+          onClick={switchAuthModeHandler}
+          btnType="Danger"
+        >
+          {isSignup ? 'Switch To Login' : 'Sign Up!'}
+        </StyledLoginSwitchButton>
+        <StyledForm onSubmit={submitHandler}>
+          {formElements}
+          <StyledSubmitButtonContainer>
+            <Button
+              isDarkText={true}
+              text={isSignup ? 'Continue' : 'Login'}
+            ></Button>
+          </StyledSubmitButtonContainer>
+        </StyledForm>
+        <StyledCloseButtonContainer>
+          <Button
+            isDarkText={true}
+            callback={() => toggleCallback()}
+            text={'X'}
+          />
+        </StyledCloseButtonContainer>
+      </StyledDrawElementsWrapperForAnimation>
     </StyledDrawContainer>
   );
 };
